6b9127be8fcdac8fa1e2ebc7cc39422bd924e0a7
Mauro Lepore <maurolepore@gmail.com>
2024-10-22 20:43:58 +0000

    site

--- R/greet.R
+++ R/greet.R
@@ -1,3 +1,13 @@
+#' Greet
+#'
+#' @param name String. Your name.
+#'
+#' @return String
+#' @export
+#'
+#' @examples
+#' greet()
+#' greet("mauro")
 greet <- function(name = NULL) {
   name <- name %||% "world"
   paste("hello", name)
